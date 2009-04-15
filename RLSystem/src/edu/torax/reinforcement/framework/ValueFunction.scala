159b0bda6ac6129473700a4f250bf3ed6e3e0057
nakryjko <nakryjko@bd86dab0-26ca-11de-9ca7-1beefeb68fa5>
2009-04-15 23:28:12 +0000

    Some problems with SVN... :(

--- RLSystem/src/edu/torax/reinforcement/framework/ValueFunction.scala
+++
@@ -1,6 +0,0 @@
-package edu.torax.reinforcement.framework
-
-trait ValueFunction {
-  def update(state: State, action: Action, delta: Double): Unit
-  def apply(state: State, action: Action): Double
-}
